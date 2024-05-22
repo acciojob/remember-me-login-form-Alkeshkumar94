9c5dbe676e5c07e8b5f76b9110782439aa29763d
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-05-22 14:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const rememberMeCheckbox = document.getElementById("checkbox");
+const existingUserBtn = document.getElementById("existing");
+
+const saveUsername=localStorage.getItem('username');
+const savePassword=localStorage.getItem('password');
+
+if(savePassword && saveUsername){
+	 existingUserBtn.style.display = "block";
+}
